bc97b492036176b7a443d91c7541cf2a1c593b87
Andres Velasquez <andresvel00@gmail.com>
2020-12-13 19:38:48 -0500

    User Registration

---
+++ app/src/main/java/com/avh/socialapp/ProfileActivity.java
@@ -0,0 +1,20 @@
+package com.avh.socialapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        // ActionBar and title
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Profile");
+
+    }
+}
--- app/src/main/java/com/avh/socialapp/RegisterActivity.java
+++ app/src/main/java/com/avh/socialapp/RegisterActivity.java
@@ -1,14 +1,25 @@
 package com.avh.socialapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -18,6 +29,9 @@ public class RegisterActivity extends AppCompatActivity {
 
     // Progress Bar to display while registering user
     ProgressDialog progressDialog;
+
+    // Declare an instance of FirebaseAuth
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +53,8 @@ public class RegisterActivity extends AppCompatActivity {
 
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Registering user...");
+
+        mAuth = FirebaseAuth.getInstance();
 
         // Handle Register Btn Click
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
@@ -68,7 +84,32 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void registerUser(String email, String password) {
         // Email and password patterns are valid, show progress dialog and start registering user
-        
+        progressDialog.show();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, dismiss dialog and start register activity
+                            progressDialog.dismiss();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(RegisterActivity.this, "Registered...\n" + user.getEmail(), Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, ProfileActivity.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            progressDialog.dismiss();
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Error, dismiss progress dialog and get and show the error message
+                progressDialog.dismiss();
+                Toast.makeText(RegisterActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
